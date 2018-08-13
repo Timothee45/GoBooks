441ca8d3929a6da9787da3609dd19ae30ce677d7
tmasurel <tmasurel@norauto.com>
2018-08-13 13:26:02 +0200

    Middleware added

--- books/error/error.go
+++ books/error/error.go
@@ -6,6 +6,7 @@ type Error struct {
 }
 
 var IncorrectId = Error{Code: 415, Label: "Incorrect id"}
+var BadToken = Error{Code: 409, Label: "Bad Token"}
 
 func GenerateError(code int, label string) Error {
 	return Error{Code: code, Label: label}
--- books/route/route.go
+++ books/route/route.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"log"
 	"books/handler"
+	"books/error"
+    "encoding/json"
+    "fmt"
 )
 
 func InitRoutes() {
@@ -18,5 +21,26 @@ func InitRoutes() {
 
 	router.HandleFunc("/books/type/{type}", handler.GetBooksByType).Methods("GET")
 
+	router.Use(Middleware)
+
     log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func Middleware(h http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    	token := r.Header.Get("user")
+
+    	if token != "" {
+    		resp, err := http.Get("http://localhost:8080/?user=" + token)
+
+    		fmt.Println(resp.Body)
+
+    		if err == nil {
+    			log.Println("middleware", r.URL)
+        		h.ServeHTTP(w, r)
+    		}
+    	} else {
+    		json.NewEncoder(w).Encode(error.BadToken)
+    	}
+    })
 }
